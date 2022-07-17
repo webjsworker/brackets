57609d1996b266de273c47a98e4d316d89389750
Aliaksei <aliakseiyudziankou@gmail.com>
2022-07-17 23:48:48 +0300

    feat: part of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  /* return true */
+  /* console.log(str)
+  console.log(bracketsConfig) */
+  let result = true ;
+    let value = bracketsConfig.map( (elem) => {
+     /*  console.log(elem) */
+    let start = elem[0]
+    let end = elem[1]
+    const creatRegExp = new RegExp( `[${start}${end}]`, 'gi' );
+    const replace = "";
+    let  answer = str.replace(creatRegExp, replace);
+    /* console.log(!answer.length) */
+    /* return !answer.length */
+    result = !answer.length
+    }
+  )
+  return result 
 }
