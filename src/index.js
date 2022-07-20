15a06d7420ae7e8ceab7f42119f79e873b16a941
Aliaksei <aliakseiyudziankou@gmail.com>
2022-07-20 21:40:07 +0300

    refactor : add part of solutions

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  /* console.log(str)
-  console.log(bracketsConfig) */
-
+  console.log(str)
+  console.log(bracketsConfig)
   let result = true;
   let strok = str;
   let answer = ''
   function Replaser(elem) {
     let start = elem[0]
     let end = elem[1]
-    const creatRegExp = new RegExp(`\\${start}\\${end}`, 'gi');
-    /* console.log("regexp = " + creatRegExp) */
+    const creatRegExp = new RegExp(`\\${start}\\${end}`,'g');
+    console.log("regexp = " + creatRegExp)
     const replace = "";
     answer = strok.replace(creatRegExp, replace);
     strok = answer
-    /* console.log('answer = ' + answer + answer.length) */
+    console.log('answer = ' + answer + answer.length)
     result = !answer.length
   }
-  for (let i = 0; i <= bracketsConfig.length + 3; i++) {
+  for (let i = 0; i <= str.length ; i++) {
     let value = bracketsConfig.map((element) => {
       Replaser(element)
     })
